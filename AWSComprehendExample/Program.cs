e57935fafa95d7effba412f9a2f77784ea8041d0
Jaco Brink <jaco.brink@xero.com>
2023-05-06 11:58:51 +1200

    initial commit

---
+++ AWSComprehendExample/Program.cs
@@ -0,0 +1,117 @@
+﻿using Amazon;
+using Amazon.Comprehend;
+using Amazon.Comprehend.Model;
+using System.Diagnostics;
+
+// Replace with your python exe path
+string pythonExe = "C:\\Users\\YourUserName\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+// Replace with the path where you want to create code projects
+string actionOutputDirectory = "C:\\MyCodeRootPath";
+
+// Replace with your aws cli profile name
+AWSConfigs.AWSProfileName = "your-aws-profile";
+var comprehendClient = new AmazonComprehendClient();
+
+string[] projectTypes = { "console", "webapi", "wpf", "sln" };
+string[] verbs = { "create", "add", "remove" };
+
+while (true)
+{
+    Console.WriteLine("\nEnter your prompt: ");
+    var prompt = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(prompt))
+        break;
+
+    DetectSyntaxResponse detectedSyntaxResponse = await GetChomprehensionAnalysis(comprehendClient, prompt);
+
+    // Initialize variables to hold the attribute values
+    string action = "";
+    string type = "";
+    string name = "";
+
+    foreach (var syntaxToken in detectedSyntaxResponse.SyntaxTokens)
+    {
+        ProcessSyntaxToken(ref action, ref type, ref name, syntaxToken);
+    }
+
+    // Print the attribute values
+    Console.WriteLine("Action: " + action);
+    Console.WriteLine("Type: " + type);
+    Console.WriteLine("Name: " + name);
+
+    Console.WriteLine("\nWould you like to proceed with these values?: y/n");
+    prompt = Console.ReadLine();
+
+    if (prompt != "y")
+        break;
+
+    ProcessAction(action, type, name);
+}
+
+async Task<DetectSyntaxResponse> GetChomprehensionAnalysis(AmazonComprehendClient comprehendClient, string? prompt)
+{
+    var detectedSyntaxRequest = new DetectSyntaxRequest
+    {
+        LanguageCode = SyntaxLanguageCode.En,
+        Text = prompt
+    };
+    var detectedSyntaxResponse = await comprehendClient.DetectSyntaxAsync(detectedSyntaxRequest);
+    return detectedSyntaxResponse;
+}
+
+void ProcessSyntaxToken(ref string action, ref string type, ref string name, SyntaxToken syntaxToken)
+{
+    var partOfSpeechTag = syntaxToken.PartOfSpeech.Tag;
+    var syntaxTextLower = syntaxToken.Text.ToLower();
+
+    if (partOfSpeechTag == PartOfSpeechTagType.VERB)
+    {
+        // thinks like "name it", "call it" need to be ignored
+        if (verbs.Contains(syntaxTextLower))
+        {
+            action = syntaxToken.Text;
+        }
+    }
+
+    if (partOfSpeechTag == PartOfSpeechTagType.NOUN)
+    {
+        foreach (var projectType in projectTypes)
+        {
+            if (syntaxTextLower.Contains(projectType))
+            {
+                type = projectType;
+                break;
+            }
+        }
+    }
+
+    if (partOfSpeechTag == PartOfSpeechTagType.PROPN)
+    {
+        // Preserve casing
+        name = syntaxToken.Text;
+    }
+}
+
+void ProcessAction(string action, string type, string name)
+{    
+    string script = "dotnet-helper.py";
+    string arguments = $"{action} {name} {type} {actionOutputDirectory}";
+
+    ProcessStartInfo start = new ProcessStartInfo();
+    start.FileName = pythonExe;
+    start.Arguments = $"{script} {arguments}";
+    start.UseShellExecute = false;
+    start.RedirectStandardOutput = true;
+
+    using (Process process = Process.Start(start))
+    {
+        using (StreamReader reader = process.StandardOutput)
+        {
+            string result = reader.ReadToEnd();
+            Console.WriteLine(result);
+        }
+        process.WaitForExit();
+    }
+}
---
+++ AWSComprehendExample/dotnet-helper.py
@@ -0,0 +1,23 @@
+﻿import subprocess
+import sys
+import os
+
+
+action = sys.argv[1]
+name = sys.argv[2]
+projectType = sys.argv[3]
+workingDirectory = sys.argv[4]
+
+os.chdir(workingDirectory)
+
+if action == "create":
+    subprocess.run(["dotnet", "new", projectType, "-n", name])
+    print(f"Successfully created {projectType} project named {name}")
+elif action == "add":
+    subprocess.run(["dotnet", "sln", "add", name + "." + projectType + ".csproj"])
+    print(f"Successfully added {projectType} project named {name} to the solution")
+elif action == "remove":
+    subprocess.run(["dotnet", "remove", "reference", name + "." + projectType + ".csproj"])
+    print(f"Successfully removed {projectType} project named {name} from the solution")
+else:
+    print("Invalid action provided")
